878655e57e6229f1d76602450a4f00b5c4d299b9
JoseTankian <jose.ma.16@hotmail.com>
2015-11-28 17:43:38 +0100

    CreacionCapaYActivadorBoton[Jose2.6]

---
+++ Reapy el Becario/Assets/Scripts/ActivadorBoton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivadorBoton : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionStay2D(Collision2D objeto)
+	{
+		if (objeto.transform.tag == "Gordo")
+		{
+			Debug.Log ("El botonFunciona!!!!!");
+		}
+	}
+}
