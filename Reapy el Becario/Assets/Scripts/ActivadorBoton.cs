84daeecfbb2aa34f4780d1e41ef1e6cb49eb6b5c
JoseTankian <jose.ma.16@hotmail.com>
2015-11-28 18:37:53 +0100

    ActivadorBotonHecho[Jose3]

--- Reapy el Becario/Assets/Scripts/ActivadorBoton.cs
+++ Reapy el Becario/Assets/Scripts/ActivadorBoton.cs
@@ -4,23 +4,37 @@ using System.Collections;
 public class ActivadorBoton : MonoBehaviour 
 {
 
+	Animator boton;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		boton = GetComponent<Animator>(); //animacion boton
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
-	}
 
+		
+	}
+		
+	
 	void OnCollisionStay2D(Collision2D objeto)
 	{
 		if (objeto.transform.tag == "Gordo")
 		{
+			boton.SetBool("gordo",true);
 			Debug.Log ("El botonFunciona!!!!!");
 		}
 	}
+
+
+	void OnCollisionExit2D(Collision2D objeto) {
+
+		if (objeto.transform.tag == "Gordo")
+		{
+			boton.SetBool("gordo",false);
+		}
+	}
+	
 }
