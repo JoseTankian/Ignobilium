fee933b91968eb9352b7f37b88e9ea90b88f210c
JoseTankian <jose.ma.16@hotmail.com>
2015-11-29 15:19:45 +0100

    Particulas[Jose2.7]

---
+++ Reapy el Becario/Assets/Scripts/ActivadorBlabla.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivadorBlabla : MonoBehaviour {
+
+	public GameObject particulasblabla;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+	void OnTriggerEnter2D(Collider2D objeto)
+	{
+		if (objeto.transform.tag == "GordoFisica")
+		{
+
+			Instantiate(particulasblabla, transform.position, transform.rotation);
+
+		}
+	}
+	
+	
+	void OnTriggerExit2D(Collider2D objeto) 
+	{
+		if (objeto.transform.tag == "GordoFisica" )
+		{
+			
+		}
+	}
+}
